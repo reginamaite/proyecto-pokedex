41350c24535ea797765ab0d54206f43f14d4a1d0
regi <regina.maite.at@gmail.com>
2022-10-06 19:45:41 -0300

    server

---
+++ db.js
@@ -0,0 +1,101 @@
+
+    export const pokemon=()=> [
+        {
+            "id" : 1,
+            "name": "Bulbasaur",
+            "type": "Grass",
+            "style": "Poison",
+            "weight": "6.9 kg",
+            "height": "0.7 m",
+            "moves": "Chiorophyll Overgrow",
+            "description": "There is a plant seed on its back right from the day this Pokémon is born, The seed slowly grows larger."
+        }
+        ,
+        {
+            "id" : 152,
+            "name": "Mew",
+            "type": "Psychic",
+            "style": "",
+            "weight": "4.0 kg",
+            "height": "0.4 m",
+            "moves": "Synchronized",
+            "description": "When view through a microscope this Pokemon's short, fine, delicate hair can be seen."
+        }
+        ,
+        {
+            "id" : 25,
+            "name": "Pikachu",
+            "type": "Electric",
+            "style": "",
+            "weight": "6.0 kg",
+            "height": "0.4 m",
+            "moves": "Mega-Punch Pay-Day",
+            "description": "Pikachu that can generate powerful electricity have cheek sacs that are extra soft and super stretchy."
+        }
+        ,
+        {
+            "id" : 4,
+            "name": "Charmander",
+            "type": "Fire",
+            "style": "",
+            "weight": "8.5 kg",
+            "height": "0.6 m",
+            "moves": "Mega-Punch Fire-Punch",
+            "description": "It has a preference for hot things. When it rains, steam is said to sput from the tip of it's tail"
+        }
+        ,
+        {
+            "id" : 7,
+            "name": "Squirtle",
+            "type": "Water",
+            "style": "",
+            "weight": "9.0 kg",
+            "height": "0.5 m",
+            "moves": "Torrent Rain-Dish",
+            "description": "When it retracts it's long neck into it's shell, it squirts out water with vigorous force."
+        }
+        ,
+        {
+            "id" : 92,
+            "name": "Gastly",
+            "type": "Ghost",
+            "style": "Type",
+            "weight": "0.1 kg",
+            "height": "1,3 m",
+            "moves": "Levitate",
+            "description": "Born from gases, anyone would faint if engulfed by it's gaseous body, which contains poison."
+        }
+        ,
+        {
+            "id" : 132,
+            "name": "Ditto",
+            "type": "Normal",
+            "style": "",
+            "weight": "4.0 kg",
+            "height": "0.3 m",
+            "moves": "Limber Imposter",
+            "description": "It can reconstitute it's entire cellular structure to change into what it sees, but it returns to normal when it relaxes."
+        }
+        ,
+        {
+            "id" : 12,
+            "name": "Butterfree",
+            "type": "Bug",
+            "style": "Flying",
+            "weight": "32.0 kg",
+            "height": "1.1 m",
+            "moves": "Compound-Eyes Tinted-Lens",
+            "description": "In battle, it flaps it's wings at great speed to release highly toxic dust into the air."
+        }
+        ,
+        {
+            "id" : 304,
+            "name": "Aron",
+            "type": "Steel",
+            "style": "Rock",
+            "weight": "60.0 kg",
+            "height": "0,4 m",
+            "moves": "Sturdy Rock-Head",
+            "description": "It eats iron ore - and sometimes railroad tracks - to build up the steel armor that protects it's body."
+        }
+    ]
